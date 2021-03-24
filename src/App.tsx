5457df890b9bde60e96b8f8c792fa69a583c2b2d
amagiddmxh <amagiddmxh@qq.com>
2021-03-24 08:45:47 +0800

    [ Monitor ] Integrate sentry

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,9 @@ import About from './pages/About'
 import './App.css'
 
 function App() {
-  
   return (
     <Router>
       <Header name="AmagiDDmxh" />
-      {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-      */}
       <Page />
     </Router>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: "https://863cce5b4e8a4e45903394ff3db7d60c@o557168.ingest.sentry.io/5689072",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
