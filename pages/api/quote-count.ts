139b7f5c9ad0e3439aa94d8881c5370ed18e0dcf
Amagi:DDmxh <amagiddmxh@qq.com>
2021-04-27 01:46:24 +0800

    Add GET quote-count

---
+++ pages/api/quote-count.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma'
+import { randomInteger } from '../../lib/random'
+
+// GET /api/quote-count
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method?.toLowerCase() !== 'get') {
+    return res.status(501)
+  }
+
+  const quotes = await prisma.quote.findMany()
+  const viewCount = quotes.reduce((sum, { viewCount }) => sum + viewCount, 0)
+  res.json(viewCount)
+}
