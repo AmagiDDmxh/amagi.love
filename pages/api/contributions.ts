3743c59120514567e1e14af3188b8cc70be62ead
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-12 21:31:11 +0800

    Update contribution API

--- pages/api/contributions.ts
+++ pages/api/contributions.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const { GITHUB_TOKEN = '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', GITHUB_USERNAME = 'amagiddmxh' } = process.env
+const { GITHUB_TOKEN, GITHUB_USERNAME = 'amagiddmxh' } = process.env
 
 interface ContributionDay {
   color: string
@@ -13,11 +13,11 @@ interface Week {
 }
 
 interface StatData {
-  uv: number
-  name: string
+  contributionCount: number
+  date: string
 }
 
-// GET /api/quote-count
+// GET /api/contributions
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method?.toLowerCase() !== 'get') {
     return res.status(501)
@@ -26,35 +26,49 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   const contributionsResponse = await getContributions()
   const total = contributionsResponse.data.user.contributionsCollection.contributionCalendar.totalContributions
   const works: StatData[] = contributionsResponse.data.user.contributionsCollection.contributionCalendar.weeks.flatMap(
-    (week: Week) => week.contributionDays.map(({ contributionCount, date }) => ({ name: date, uv: contributionCount })),
+    (week: Week) =>
+      week.contributionDays.map(({ contributionCount, date }) => ({
+        date: new Date(date).getTime(),
+        contributionCount,
+      })),
   )
 
   const normalizedWorks = []
-  let growth = works[0].uv
-  let base = works[0].uv > 0 ? 1 : -10
-  // const average = works.reduce((acc, { uv }) => acc + uv, 0) / works.length
-  for (let i = works.length - 200 + 1; i < works.length; i++) {
+  // let base = works[0].contributionCount > 0 ? 1 : -10
+  const average = works.reduce((acc, { contributionCount }) => acc + contributionCount, 0) / works.length
+  const around = 7
+  for (let i = works.length -183; i < works.length - around; i++) {
     const work = works[i]
-    const prevWork = works[i - 1] ?? { uv: 0 }
-    const currLength = i + 1
-    const { uv: currVal } = work
-    const { uv: prevVal } = prevWork
+    const recentAverage = getRecentAverage(works, i, around)
+    const { contributionCount: currVal, date } = work
     // const division = prevVal === 0 ? 1 : prevVal
 
     // const uv = (currVal / prevVal) ** (1 / (currLength - 1))
     // growth = (currVal - growth) / growth
     // const uv = currVal / prevVal - 1
-    base = base + (currVal ? 1 : -10)
 
-    base = normalizedWorks.push({
-      name: work.name,
+    normalizedWorks.push({
+      date,
+      currVal,
+      recentAverage,
+      average,
       // uv,
       // uv: currVal - prevVal,
       // uv: growth,
-      uv: base,
+      uv: (currVal - recentAverage),
     })
   }
-  res.json({ data: normalizedWorks, total })
+  res.json({ data: normalizedWorks, total, average })
+}
+
+function getRecentAverage(works: StatData[], index: number, around = 7) {
+  const interval = Math.floor(around / 2)
+  let recentAverage = 0
+  for (let i = index - interval; i <= index + interval; i++) {
+    const work = works[i]
+    recentAverage += work.contributionCount / around
+  }
+  return recentAverage
 }
 
 async function getContributions(token = GITHUB_TOKEN, username = GITHUB_USERNAME) {
@@ -62,25 +76,24 @@ async function getContributions(token = GITHUB_TOKEN, username = GITHUB_USERNAME
     Authorization: `bearer ${token}`,
   }
   const body = {
-    query: `query {
-          user(login: "${username}") {
-            name
-            contributionsCollection {
-              contributionCalendar {
-                colors
-                totalContributions
-                weeks {
-                  contributionDays {
-                    contributionCount
-                    date
-                    color
-                  }
-                  firstDay
-                }
+    query: `
+    query {
+      user(login: "${username}") {
+        name
+        contributionsCollection {
+          contributionCalendar {
+            totalContributions
+            weeks {
+              contributionDays {
+                contributionCount
+                date
               }
+              firstDay
             }
           }
-        }`,
+        }
+      }
+    }`,
   }
   const response = await fetch('https://api.github.com/graphql', {
     method: 'POST',
