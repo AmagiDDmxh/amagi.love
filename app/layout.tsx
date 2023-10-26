8e475509a17e107c9eacdfef1c09bb8d1063aa66
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-10-26 15:17:00 +0800

    fetch quote in the client side using swr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import '#/styles/globals.css'
 import Header from '#/components/Header'
-import Quote from '#/components/Quote'
-import { type Quote as QuoteType } from '#/typings/quote'
-import { notFound } from 'next/navigation'
-import { getBaseUrl } from '#/lib/getBaseUrl'
 
 export const metadata = {
   title: 'Amagi',
@@ -11,33 +7,12 @@ export const metadata = {
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const quoteProps = await getQuoteProps()
-
   return (
     <html lang="en">
       <body>
         <Header />
-        <Quote {...quoteProps} />
         {children}
       </body>
     </html>
   )
-}
-
-async function getQuoteProps() {
-  const res = await fetch(`${getBaseUrl()}/api/quote`)
-
-  if (!res.ok) {
-    // Render the closest `error.js` Error Boundary
-    throw new Error('Something went wrong!')
-  }
-
-  const quoteProps = (await res.json()) as QuoteType
-
-  if (!quoteProps) {
-    // Render the closest `not-found.js` Error Boundary
-    notFound()
-  }
-
-  return quoteProps
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import Quote from '#/components/Quote'
+import { type Quote as QuoteType } from '#/typings/quote'
+import useSWR from 'swr'
+
 export default function HomePage() {
+  const { data: quote } = useSWR<QuoteType>('/api/quote')
+
   return (
-    <div className="pl-2">
-      <h1 className="text-black">Amagi&apos;s Home</h1>
-      <p className="text-black">Oops, quite a nasty room. Other than that, nothings here.</p>
-    </div>
+    <>
+      {quote && <Quote {...quote} />}
+      <div className="pl-2">
+        <h1 className="text-black">Amagi&apos;s Home</h1>
+        <p className="text-black">Oops, quite a nasty room. Other than that, nothings here.</p>
+      </div>
+    </>
   )
 }
